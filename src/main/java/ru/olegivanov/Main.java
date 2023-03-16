5ee0d9fd85bdfb5cf5b90fa916ed4c52121952e3
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-03-16 23:35:42 +0400

    Look like done: v1

--- src/main/java/ru/olegivanov/Main.java
+++ src/main/java/ru/olegivanov/Main.java
@@ -10,15 +10,19 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) throws IOException {
         int port = 8080;
-        try(ServerSocket serverSocket = new ServerSocket(port);
-            // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
-            Socket clientSocket = serverSocket.accept(); // ждем подключения
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))){
-            out.println("GET / HTTP/1.1\n" +
-                    "Host: netology.ru\n\n\n");
-            String resp = in.readLine();
-            out.println(resp);
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+             Socket clientSocket = serverSocket.accept(); // ждем подключения
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+//            out.println("GET / HTTP/1.1\n" + "Host: netology.ru\n\n\n");
+//            String resp = in.readLine();
+//            out.println(resp);
+            System.out.println("New connection accepted");
+            final String name = in.readLine();
+            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+        }catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
